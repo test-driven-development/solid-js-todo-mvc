a99ee5e4ee1a5142b66a97186ad7ff83808b5772
wil pannell <wil.pannell@wils-MacBook-Pro.local>
2022-11-08 05:59:59 -0800

    stub mvc todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+// noinspection JSXNamespaceValidation
 
+const App = () => {
   return (
+    <section class="todoapp">
+      <header class="header">
+        <h1>Todos</h1>
+        <input type="" class="new-todo" placeholder="What to do?" />
       </header>
+    </section>
   )
 }
 export default App
