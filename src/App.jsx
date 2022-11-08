f707ea54986c499fec82803208a44d00ea46cd78
wil pannell <wil.pannell@wils-MacBook-Pro.local>
2022-11-08 06:25:27 -0800

    introduce Dummy todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 // noinspection JSXNamespaceValidation
 
-import {createSignal} from 'solid-js'
+import {createSignal, For, Show} from 'solid-js'
 
 const App = () => {
-  const [todos, setTodos] = createSignal([])
+  const [todos, setTodos] = createSignal([
+    {
+      id: 1,
+      title: 'Dummy todo',
+      completed: false,
+    },
+  ])
 
+  // noinspection JSValidateTypes
   return (
     <section class="todoapp">
       <header class="header">
         <h1>Todos</h1>
         <input type="" class="new-todo" placeholder="What to do?" />
       </header>
+
+      <Show when={todos().length > 0} keyed>
+        <ul class="todo-list">
+          <For each={todos()}>
+            {todo => (
+              <li class="todo">
+                <div class="view">
+                  <label>{todo.title}</label>
+                </div>
+              </li>
+            )}
+          </For>
+        </ul>
+      </Show>
     </section>
   )
 }
