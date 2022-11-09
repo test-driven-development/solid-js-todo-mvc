bd5f210aab9ceb04cbe7a025df2002700a081c22
wil pannell <wil.pannell@wils-MacBook-Pro.local>
2022-11-09 00:06:33 -0800

    stub add UI event

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,37 @@ const App = () => {
     },
   ])
 
+  const ENTER_KEY = 13
+  const ESCAPE_KEY = 25
+  let counter = 0
+
+  const addTodo = event => {
+    const title = event.target.value.trim()
+    if (event.keyCode === ENTER_KEY && title) {
+      setTodos(todos => {
+        return [
+          ...todos,
+          {
+            id: counter++,
+            title,
+            completed: false,
+          },
+        ]
+      })
+    }
+  }
+
   // noinspection JSValidateTypes
   return (
     <section class="todoapp">
       <header class="header">
         <h1>Todos</h1>
-        <input type="" class="new-todo" placeholder="What to do?" />
+        <input
+          type="text"
+          class="new-todo"
+          placeholder="What to do?"
+          onKeyDown={addTodo}
+        />
       </header>
 
       <Show when={todos().length > 0} keyed>
